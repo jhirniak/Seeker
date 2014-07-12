8f1a3928dafc13c5c2bd88529960b6c64ff60300
Jaroslaw Hirniak <j@hirniak.info>
2014-07-12 12:11:44 +0100

    Added: alerts to modal.

--- Code/app/scripts/controllers/selector.js
+++ Code/app/scripts/controllers/selector.js
@@ -38,4 +38,21 @@ angular.module('seekerApp')
 
         $scope.selected = '';
 
+        // alerts
+
+        $scope.alerts = [
+            { type: 'warning', msg: 'Lorem ipsum...'},
+            { type: 'info', msg: 'Lorem ipsum...'},
+            { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+            { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+        ];
+
+        $scope.addAlert = function() {
+            $scope.alerts.push({msg: 'Another alert!'});
+        };
+
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
+
 });
